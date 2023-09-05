bba3a1103297fe80ea0a45842bb95b893680050e
sh0von <minarshovon909@gmail.com>
2023-09-05 21:28:00 +0600

    Color Update

--- components/NotFound.js
+++ components/NotFound.js
@@ -3,8 +3,8 @@ import React from 'react';
 
 const NotFound = () => {
   return (
-    <div className="container my-5">
-      <div className="d-flex flex-column align-items-center justify-content-center">
+    <div className="container mt-5">
+      <div className="d-flex flex-column align-items-center justify-content-center" style={{ minHeight: 'calc(100vh - 200px)' }}>
         <h1 className="text-center">404 - Page Not Found</h1>
         <p className="text-center">
           The page you are looking for doesn't exist or has been moved.
