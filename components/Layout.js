e5f8d37085dfd7b0c8ab1beb5b01855674408bd4
sh0von <minarshovon909@gmail.com>
2023-09-08 11:35:07 +0600

    Project

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+import { Analytics } from '@vercel/analytics/react';
 function Layout({ children }) {
   return (
     <div className="container-fluid">
-      <main>{children}</main>
+      <main>{children} <Analytics /></main>
       <footer className="mt-4">
         <p className="text-center">
           &copy; {new Date().getFullYear()} sh0von.me
