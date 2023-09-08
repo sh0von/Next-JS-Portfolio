8f979830b231c04eba559320e619d8969e2eaa4b
sh0von <minarshovon909@gmail.com>
2023-09-08 23:37:19 +0600

    New sec

--- pages/index.js
+++ pages/index.js
@@ -61,16 +61,13 @@ function Skill({ name, percentage }) {
 }
 
 function Home() {
-
   const [displayedProjects, setDisplayedProjects] = useState(3);
 
   const loadMoreProjects = () => {
-   
     setDisplayedProjects(displayedProjects + 3);
   };
   return (
     <Layout>
-      
       <section className="mt-4 bounce">
         <div className="container">
           <div className="row">
@@ -81,21 +78,21 @@ function Home() {
         </div>
       </section>
       <section className="mt-4  bounce">
-  <div className="container">
-    <div className="row">
-      <div className="col-md-12">
-        <p className="description-text" style={{ textAlign: "center" }}>
-          <br />
-          I'm a passionate developer with a love for coding and a creative
-          mind. I enjoy working on projects that challenge me and push my
-          boundaries. When I'm not coding, you can find me exploring new
-          technologies or working on personal projects.
-        </p><br></br>
-      </div>
-    </div>
-  </div>
-</section>
-
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <p className="description-text" style={{ textAlign: "center" }}>
+                <br />
+                I'm a passionate developer with a love for coding and a creative
+                mind. I enjoy working on projects that challenge me and push my
+                boundaries. When I'm not coding, you can find me exploring new
+                technologies or working on personal projects.
+              </p>
+              <br></br>
+            </div>
+          </div>
+        </div>
+      </section>
 
       <section className="mt-4 box bounce">
         <div className="container">
@@ -125,122 +122,112 @@ function Home() {
                     {language.name} - {language.proficiency}
                   </span>
                 ))}
-              </div><br></br>
-              <div className=" ">
-              <h2 className="section-heading">Framework</h2>
-              <div className="d-flex flex-wrap">
-                {framework.map((framework, index) => (
-                  <span
-                    key={index}
-                    className="custom-tag"
-                    style={{
-                      background: getRandomGradientColor(),
-                      marginBottom: "5px",
-                    }}
-                  >
-                    {framework.name} - {framework.proficiency}
-                  </span>
-                ))}
               </div>
-
-
-              
-            </div>
-
-
+              <br></br>
+              <div className=" ">
+                <h2 className="section-heading">Framework</h2>
+                <div className="d-flex flex-wrap">
+                  {framework.map((framework, index) => (
+                    <span
+                      key={index}
+                      className="custom-tag"
+                      style={{
+                        background: getRandomGradientColor(),
+                        marginBottom: "5px",
+                      }}
+                    >
+                      {framework.name} - {framework.proficiency}
+                    </span>
+                  ))}
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </section>
 
       <section className="mt-4 box">
-  <div className="container scale-up">
-    <h2 className="section-heading">CTF Achievements</h2>
-    <div className="row">
-      {ctfAchievements.map((achievement, index) => (
-        <div className="col-md-4 mb-4" key={index}>
-          <div
-            className="card ctf-card" 
-            style={{
-              background: getRandomGradientColor(),
-              color: "#fff",
-              position: "relative",
-              padding: "15px",
-              borderRadius: "10px",
-              boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
-            }}
-          >
-            {}
-            <div className="ctf-position-background">
-              {achievement.position}
-            </div>
+        <div className="container scale-up">
+          <h2 className="section-heading">CTF Achievements</h2>
+          <div className="row">
+            {ctfAchievements.map((achievement, index) => (
+              <div className="col-md-4 mb-4" key={index}>
+                <div
+                  className="card ctf-card"
+                  style={{
+                    background: getRandomGradientColor(),
+                    color: "#fff",
+                    position: "relative",
+                    padding: "15px",
+                    borderRadius: "10px",
+                    boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+                  }}
+                >
+                  {}
+                  <div className="ctf-position-background">
+                    {achievement.position}
+                  </div>
 
-            <div className="card-body">
-              <div className="ctf-icon">
-                <i className="fa fa-bug"></i>
+                  <div className="card-body">
+                    <div className="ctf-icon">
+                      <i className="fa fa-bug"></i>
+                    </div>
+                    <h3 className="card-title">{achievement.name}</h3>
+                    <p className="card-text">{achievement.date}</p>
+                  </div>
+                </div>
               </div>
-              <h3 className="card-title">{achievement.name}</h3>
-              <p className="card-text">{achievement.date}</p>
-            </div>
+            ))}
           </div>
         </div>
-      ))}
-    </div>
-  </div>
-</section>
+      </section>
 
-
-
-<section className="mt-4 box">
+      <section className="mt-4 box">
         <div className="container scale-up">
           <h2 className="section-heading">My Project</h2>
           <br />
           <div className="row masonary">
             {projects.slice(0, displayedProjects).map((project, index) => (
-       <div className="col-md-4 mb-4 slide" key={index}>
-       <a
-         href={project.link}
-         className="a"
-         target="_blank"
-         rel="noopener noreferrer"
-       >
-         <div
-           className="card project-card"
-           style={{
-             background: getRandomGradientColor(),
-           }}
-         >
-           <div className="overlay"></div> {}
-           <div className="card-body">
-             <h5 className="card-title">
-               {project.title}
-               <span className="external-link-icon">
-                 <i className="fa fa-link"></i>
-               </span>
-             </h5>
-             <p className="card-text">{project.description}</p>
-     
-             {}
-             {project.image && (
-               <img
-                 src={project.image}
-                 alt={project.title}
-                 className="project-image"
-               />
-             )}
-           </div>
-         </div>
-       </a>
-     </div>
-     
+              <div className="col-md-4 mb-4 fadein" key={index}>
+                <a
+                  href={project.link}
+                  className="a"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <div
+                    className="card project-card"
+                    style={{
+                      background: getRandomGradientColor(),
+                    }}
+                  >
+                    <div className="overlay"></div> {}
+                    <div className="card-body">
+                      <h5 className="card-title">
+                        {project.title}
+                        <span className="external-link-icon">
+                          <i className="fa fa-link"></i>
+                        </span>
+                      </h5>
+                      <p className="card-text">{project.description}</p>
+
+                      {}
+                      {project.image && (
+                        <img
+                          src={project.image}
+                          alt={project.title}
+                          className="project-image"
+                        />
+                      )}
+                    </div>
+                  </div>
+                </a>
+              </div>
             ))}
           </div>
           {displayedProjects < projects.length && (
             <div className="text-center mt-4">
-              <button
-                className="load-more-button "
-                onClick={loadMoreProjects}
-              >
+              <button className="load-more-button " onClick={loadMoreProjects}>
                 Load More <i className="fa fa-arrow-down"></i>
               </button>
             </div>
