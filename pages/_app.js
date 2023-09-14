0577372a1270f5dca834813885929009e5202166
sh0von <minarshovon909@gmail.com>
2023-09-14 19:50:06 +0600

    Ana

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,33 @@
-import "@/styles/globals.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "font-awesome/css/font-awesome.css";
+// app.js
 
-import "../styles/styles.css";
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import ReactGA from 'react-ga';
 
-import "../styles/animation.css";
-export default function App({ Component, pageProps }) {
+// Initialize Google Analytics with your Measurement ID
+ReactGA.initialize('G-7Q9W5EGHMG', {
+  debug: process.env.NODE_ENV === 'development', // Enable debug mode in development
+});
+
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Set up Google Analytics to track page changes
+    const handleRouteChange = (url) => {
+      ReactGA.set({ page: url });
+      ReactGA.pageview(url);
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    // Remove the event listener when the component is unmounted
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, []);
+
   return <Component {...pageProps} />;
 }
+
+export default MyApp;
